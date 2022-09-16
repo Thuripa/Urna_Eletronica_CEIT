81a6cffd110f9f2d7105f66d2a19d2aa2e498d04
Guilherme <guilhermemelo@protonmail.com>
2022-09-16 19:19:53 -0300

    comit #2
    corrigindo algumas exceções na busca e tirando os números de turma da lista de tokens

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Projeto Urna CEIT
+import sys
 
+
+# Função Que busca o Token na Lista
 def valida_token():
     # Atribui o arquivo txt com os tokens para a variável arquivo
     with open("lista_tokens.txt", "r") as arquivo:
@@ -7,21 +10,24 @@ def valida_token():
         # Linhas é uma lista[] onde cada elemento é uma linha do arquivo
         linhas = arquivo.readlines()
 
-        # Percorre Linhas
+        # Se encontrar o token inserido pelo usuário dentro da lista de tokens retorna VERDADEIRO e fecha o arquivo
         for linha in linhas:
-            # Se encontrar o token inserido pelo usuário dentro da lista de tokens retorna verdadeiro
-            if usuario in linha:
+            if usuario == linha.strip():
                 arquivo.close()
                 return True
+            else:
+                print(usuario, " != ", linha.strip())
+
+        # Se não encontrar o token retorna FALSO e fecha o arquivo
         arquivo.close()
         return False
 
 
-
+# Entrada do usuário (No caso seria o texto de um objeto da UI)
 usuario = input("Insira seu Token de Aluno: ")
+
 
 if valida_token():
     print("Usuário Encontrado!")
 else:
     print("Usuário Inexistente!")
-
